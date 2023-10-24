eeb13d53009cffb75beb5d326cfe3f355b655a5d
Juanjo098 <juanjo.silva.098@gmail.com>
2023-10-24 12:11:18 -0600

    feature(docentes): insert create, edit wip

--- Controllers/DocenteController.cs
+++ Controllers/DocenteController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Titulacion.Clases;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using Microsoft.VisualBasic;
 
 namespace Titulacion.Controllers
 {
@@ -51,6 +52,55 @@ namespace Titulacion.Controllers
             return View();
         }
 
+        [Authorize(Roles = "1")]
+        [Route("/Administracion/Docentes/Insertar")]
+        [HttpPost]
+        public async Task<IActionResult> Insertar(Clases.Post.Docente modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.departamentos = await ListaDepartamentos();
+                ViewBag.id_dpto = modelo.Id_Dpto;
+                ViewBag.nombre = modelo.Nombre;
+                ViewBag.titulo = modelo.Titulo;
+                ViewBag.diminutivo = modelo.Diminutivo;
+                ViewBag.cedula = modelo.Cedula;
+                return View();
+            }
+
+            try
+            {
+                if (await Existe(modelo.Cedula))
+                {
+                    ViewBag.error = true;
+                    ViewBag.id_dpto = modelo.Id_Dpto;
+                    ViewBag.nombre = modelo.Nombre;
+                    ViewBag.titulo = modelo.Titulo;
+                    ViewBag.diminutivo = modelo.Diminutivo;
+                    ViewBag.cedula = modelo.Cedula;
+                    ViewBag.departamentos = await ListaDepartamentos();
+                    return View();
+                }
+
+                Docente docente = new Docente {
+                    IdDpto = modelo.Id_Dpto,
+                    Nombre = modelo.Nombre.ToUpper(),
+                    Titulo = modelo.Titulo.ToUpper(),
+                    Diminutivo = modelo.Diminutivo.ToUpper(),
+                    Cedula = modelo.Cedula
+                };
+
+                _context.Docentes.Add(docente);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Docentes");
+            }
+            catch (InvalidOperationException ex)
+            {
+                return RedirectToAction("CustomError", "Home");
+            }
+        }
+
         [Authorize(Roles = "1,2")]
         [Route("/Administracion/Docentes/Detalles")]
         [HttpGet]
@@ -59,6 +109,54 @@ namespace Titulacion.Controllers
             return View(await DocenteDetalles(id?? 1));
         }
 
+        [Authorize(Roles = "1")]
+        [Route("/Administracion/Docentes/Editar")]
+        public async Task<IActionResult> Editar(int? id)
+        {
+            ViewBag.departamentos = await ListaDepartamentos();
+            return View(await EditarDocente(id ?? 1));
+        }
+
+        [Authorize(Roles = "1")]
+        [Route("/Administracion/Docentes/Editar")]
+        [HttpPost]
+        public async Task<IActionResult> Editar(Clases.Put.Docente modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.departamentos = await ListaDepartamentos();
+                return View(modelo.Id_Docente);
+            }
+
+            try
+            {
+                if (await Existe(modelo.Cedula))
+                {
+                    ViewBag.error = true;
+                    ViewBag.departamentos = await ListaDepartamentos();
+                    return View(modelo.Id_Docente);
+                }
+
+                Docente docente = await _context.Docentes.FindAsync(modelo.Id_Docente);
+
+                if (docente == null)
+                    return RedirectToAction("CustomError", "Home");
+
+                docente.Nombre = modelo.Nombre.ToUpper();
+                docente.Titulo = modelo.Titulo.ToUpper();
+                docente.Diminutivo = modelo.Diminutivo.ToUpper();
+                docente.Cedula = modelo.Cedula;
+                docente.IdDpto = modelo.Id_Dpto;
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Docentes");
+            }
+            catch (InvalidOperationException ex)
+            {
+                return RedirectToAction("CustomError", "Home");
+            }
+        }
 
         // Utilidades
 
@@ -118,6 +216,30 @@ namespace Titulacion.Controllers
                 ).ToListAsync();
             lista.Insert(0, new SelectListItem { Text = "Selecciona el departamento", Value = "0" });
             return lista;
+        }
+
+        private async Task<bool> Existe(string cedula)
+        {
+            return await _context.Docentes.FirstOrDefaultAsync(doc => doc.Cedula == cedula) != null;
+        }
+
+        private async Task<Clases.Put.Docente> EditarDocente(int id)
+        {
+            Docente doc = await _context.Docentes.FindAsync(id);
+
+            if (doc != null)
+            {
+                return new Clases.Put.Docente { 
+                    Id_Docente = doc.IdDocente,
+                    Id_Dpto = doc.IdDpto,
+                    Nombre = doc.Nombre,
+                    Titulo = doc.Titulo,
+                    Diminutivo = doc.Diminutivo,
+                    Cedula = doc.Cedula,
+                };
+            }
+
+            return null;
         }
     }
 }
